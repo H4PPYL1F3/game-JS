fbfcec2cd283da7a9621ef77694d785ba819b64d
João Felipe <joaofelipe103@outlook.com>
2019-07-16 09:36:26 -0300

    Criação dos arquivos

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let inputs = document.querySelectorAll('.dentro');
+
+for(let input of inputs){
+    input.onkeyup = function(evento){
+        if(evento.key === 'Enter'){
+            event.target.style.backgroundColor = event.target.value;
+        }
+    }
+
+    input.onpaste = function(evento){
+        evento.preventDefault();
+        evento.target.value;
+    }
+}
